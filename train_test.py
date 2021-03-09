d8431146023ebdd93b66b192c98835256ab946a3
Tigran Margaryan <tigranmargarian@outlook.com>
2021-03-08 23:29:52 -0500

    train and test

--- train_test.py
+++ train_test.py
@@ -66,6 +66,7 @@ def train(n_epochs, loaders, model, optimizer, criterion, use_cuda, save_path):
     
     for epoch in range(1, n_epochs+1):
         # initialize variables to monitor training and validation loss
+        print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
         train_loss = 0.0
         valid_loss = 0.0
         
@@ -116,3 +117,36 @@ def train(n_epochs, loaders, model, optimizer, criterion, use_cuda, save_path):
             
     # return trained model
     return model
+
+
+def test(loaders, model, criterion, use_cuda):
+
+    # monitor test loss and accuracy
+    test_loss = 0.
+    correct = 0.
+    total = 0.
+
+    model.eval()
+    for batch_idx, (data, target) in enumerate(loaders['test']):
+        # move to GPU
+        if use_cuda:
+            data, target = data.cuda(), target.cuda()
+        # forward pass: compute predicted outputs by passing inputs to the model
+        output = model(data)
+        # calculate the loss
+        loss = criterion(output, target)
+        # update average test loss 
+        test_loss = test_loss + ((1 / (batch_idx + 1)) * (loss.data - test_loss))
+        test_loss = test_loss/len(test_loader.dataset)
+        # convert output probabilities to predicted class
+        pred = output.data.max(1, keepdim=True)[1]
+        #print(batch_idx)
+        print(pred)
+        # compare predictions to true label
+        correct += np.sum(np.squeeze(pred.eq(target.data.view_as(pred))).cpu().numpy())
+        print(correct)
+        total += data.size(0)
+            
+    print('Test Loss: {:.6f}\n'.format(test_loss))
+
+    print('\nTest Accuracy: %2d%% (%2d/%2d)' % (100. * correct / total, correct, total))
